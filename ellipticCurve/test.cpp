3fa062b43347e46f4b7fc495e2f574f09d91a680
ykm11 <ushiromiya3@gmail.com>
2019-08-20 15:18:14 +0900

    テストの更新

--- ellipticCurve/test.cpp
+++ ellipticCurve/test.cpp
@@ -1,31 +1,36 @@
 #include<iostream>
-//#include "curve.h"
+#include "curve.h"
 #include<time.h>
 
 #include "FP.h"
-//#include<gmpxx.h>
 
-#if 1
-void benchmark() {
+
+void benchmark_fp();
+void isEqual_fp_test();
+void order_test();
+
+
+Fp EllipticCurve::a;
+Fp EllipticCurve::b;
+
+void benchmark_fp() {
+    std::cout << "[*] Fp benchmark\n";
     mpz_class p = mpz_class("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16);
     Fp::setModulo(p);
-    std::cout << Fp::modulus << "\n";
     Fp x, y, z;
 
-    x = Fp(mpz_class("115792089237316195423570985008687907852837564279074904382605163141518161494337", 10)); 
-    y = Fp(mpz_class("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16));
+    x = Fp("115792089237316195423570985008687907852837564279074904382605163141518161494337", 10); 
+    y = Fp("79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16);
 
     const int n = 100000;
     time_t begin = clock();
     for(int i = 0; i < n; i++) {
-        add(x, x, y);
+        invmod(x, Fp::modulus);
     }
     time_t end = clock();
-    printf("time = %fusec\n", (end - begin) / double(CLOCKS_PER_SEC) / n * 1e6);
+    printf("\ttime = %fusec\n", (end - begin) / double(CLOCKS_PER_SEC) / n * 1e6);
 }
-#endif
 
-/*
 void order_test() {
     mpz_class p = mpz_class("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16);
     Fp::setModulo(p);
@@ -42,28 +47,29 @@ void order_test() {
     Point R;
     R = G*n;
 
-    if (R.x == zero && R.y == one && R.z == zero) {
-        std::cout << "order test: OK" << std::endl;
+    Point O = EC.point(0, 1, 0);
+    if (R == O) {
+        std::cout << "[*] order test: OK" << std::endl;
     } else {
-        std::cout << "order test: FAILED" << std::endl;
+        std::cout << "[*] order test: FAILED" << std::endl;
     }
 }
 
-void isEqual_test() {
+void isEqual_fp_test() {
     Fp::setModulo(19);
 
     Fp x = Fp(12);
     Fp y = Fp(12 - 19);
     Fp z = Fp(-12);
 
-    std::cout << "Fp isEq test1: ";
+    std::cout << "[*] Fp isEq test1: ";
     if (x == y) {
         std::cout << "OK" << std::endl;
     } else {
         std::cout << "FAILED" << std::endl;
     }
 
-    std::cout << "Fp isEq test2: ";
+    std::cout << "[*] Fp isEq test2: ";
     if (x != z) {
         std::cout << "OK" << std::endl;
     } else {
@@ -72,9 +78,8 @@ void isEqual_test() {
 
 }
 
-*/
 int main() {
-    //order_test();
-    //isEqual_test();
-    benchmark();
+    order_test();
+    isEqual_fp_test();
+    benchmark_fp();
 }
