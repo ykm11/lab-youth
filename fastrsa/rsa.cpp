16709ff2cbe1ccaa602a699dfc34f8e7fcd0ea6a
ykm11 <ushiromiya3@gmail.com>
2019-10-24 14:04:37 +0900

    _

---
+++ fastrsa/rsa.cpp
@@ -0,0 +1,35 @@
+#include<gmpxx.h>
+#include "rsa.h"
+
+    
+void RSA::encrypt(mpz_class &c, const mpz_class &m) const { // c <- m^e mod n
+    mpz_powm(c.get_mpz_t(), m.get_mpz_t(), e.get_mpz_t(), n.get_mpz_t());
+}
+void RSA::decrypt(mpz_class &m, const mpz_class &c) const { // m <- c^d mod n
+    mpz_powm(m.get_mpz_t(), c.get_mpz_t(), d.get_mpz_t(), n.get_mpz_t());
+}
+void RSA::crt_decrypt(mpz_class &m, const mpz_class &c) const { // m <- c^d mod n
+    mpz_class mp, mq;
+
+    mpz_powm(mp.get_mpz_t(), c.get_mpz_t(), dp.get_mpz_t(), p.get_mpz_t());
+    mpz_powm(mq.get_mpz_t(), c.get_mpz_t(), dq.get_mpz_t(), q.get_mpz_t());
+#if 0
+    mpz_sub(m.get_mpz_t(), mp.get_mpz_t(), mq.get_mpz_t()); // mp - mq
+    mpz_mul(m.get_mpz_t(), m.get_mpz_t(), inv_q.get_mpz_t()); // (mp - mq) * inv_q
+    mpz_mod(m.get_mpz_t(), m.get_mpz_t(), p.get_mpz_t()); // ((mp - mq) * inv_q) % p
+    mpz_addmul(mq.get_mpz_t(), m.get_mpz_t(), q.get_mpz_t()); // mq + (((mp - mq) * inv_q) % p)*q
+    if (mq < 0) {
+        mpz_add(m.get_mpz_t(), mq.get_mpz_t(), n.get_mpz_t());
+        //m = mq + n;
+    } else {
+        m = std::move(mq);
+        //m = mq;
+    }
+#else
+    m = mq + (((mp - mq) * inv_q) % p) * q;
+    if (m < 0) {
+        m = m + n;
+    }
+#endif
+}
+
